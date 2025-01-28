e2b3da7ba6d8f327cafd9b7d5e84aaeb3f0180ed
Jessa <jessamsaquin.dev@gmail.com>
2025-01-28 23:59:54 +0800

    Eggy Time Draft, no styles

---
+++ script.js
@@ -0,0 +1,35 @@
+let timerInterval;
+let timerRemaining;
+
+function startTimer(seconds){
+    clearInterval(timerInterval);
+    timerRemaining = seconds;
+    updateDisplay();
+
+    timerInterval = setInterval(() => {
+        timerRemaining--;
+        updateDisplay();
+
+        if(timerRemaining <= 0){
+            clearInterval(timerInterval);
+            alert("Egg's ready!");
+        }
+    }, 1000);
+}
+
+function updateDisplay(){
+    let minutes = Math.floor(timerRemaining / 60);
+    let seconds = timerRemaining % 60;
+    document.getElementById("timer").innerText = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+function cancelTimer(){
+    clearInterval(timerInterval);
+    timerRemaining = 0;
+    updateDisplay();
+}
+
+document.getElementById("soft-boiled").addEventListener("click", () => startTimer(240));
+document.getElementById("medium-boiled").addEventListener("click", () => startTimer(420)); 
+document.getElementById("hard-boiled").addEventListener("click", () => startTimer(720));
+document.getElementById("cancel").addEventListener("click", cancelTimer) ;
